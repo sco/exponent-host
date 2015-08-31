25bd8eea6421f8559f263058f562e39ca749a92a
James Ide <ide@jameside.com>
2015-08-31 00:42:23 -0700

    [Webpack] Build React and other JS with NODE_ENV used when running webpack
    This means during a prod deployment we'll get NODE_ENV=production (which avoids some expensive code paths) and during local testing of the prod config we can test with __DEV__ on (NODE_ENV !== "production") just by not setting NODE_ENV to "production".

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -62,7 +62,7 @@ module.exports = [
     plugins: [
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: '"production"',
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         },
       }),
       new webpack.PrefetchPlugin('react'),
